c37e5c483e15ae3cad9e1f2e2d033c8ef43d3104
leegaarden <dlwjddnjs081723@naver.com>
2025-12-17 23:02:20 +0900

    3. DFS&BFS

--- 3. DFS&BFS/bfs.py
+++ 3. DFS&BFS/bfs.py
@@ -1,1 +1,35 @@
-asdfasdf
+from collections import deque
+
+graph = [
+	[], # 노드는 주로 1번 부터 시작하기에 0번 인덱스는 비워둠
+	[2, 3, 8], # 노드1의 인접 노드들
+	[1, 7], # 노드 2의 인접 노드들
+	[1, 4, 5], # 노드 3의 인접 노드들
+	[3, 5], # 노드 4의 인접 노드들
+	[3, 4], # 노드 5의 인접 노드들
+	[7], # 노드 6의 인접 노드
+	[2, 6, 8], # 노드 7의 인접 노드 
+	[1, 7] # 노드 8의 인접 노드 
+]
+
+# 각 노드가 방문된 정보를 표현(1차원 리스트)
+visited = [False] * 9 # 인덱스 0은 사용하지 않기 위해 9개 
+
+def bfs(graph, start, visited):
+	# bfs 구현을 위해 deque 라이브러리 사용
+	queue = deque([start])
+	# 현재 노드를 방문 처리
+	visited[start] = True
+	# 큐가 빌 때가지 반복(bfs에서는 큐이기에 재귀 함수 사용을 안 함)
+	while queue:
+		# 큐에서 하나의 원소를 뽑아 출력하기
+		v = queue.popleft()
+		print(v, end = ' ')
+		# 아직 방문하지 않은 인접한 원소들을 큐에 삽입
+		for i in graph[v]:
+			if not visited[i]:
+				queue.append(i)
+				visited[i] = True
+
+# 정의된 bfs 함수 호출
+bfs(graph, 1, visited)
